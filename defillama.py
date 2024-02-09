ab0dd0110453645d985465ca6b0c050e51e00283
Weston McDonald <weston.mcdonald@blockfills.com>
2024-02-09 16:27:47 -0600

    adding additional endpoints

--- defillama.py
+++ defillama.py
@@ -69,7 +69,98 @@ class DefiLlama():
         url = f'{self.common_base_url}/tvl/{protocol}'
         return self._handle_request(url)
 
+    # coins section
+    def current_coins(self, coins):
+        url = f'{self.common_base_url}/prices/current/{coins}'
+        return self._handle_request(url)
 
+    def historical_prices_by_timestamp(self, timestamp, coins):
+        url = f'{self.common_base_url}/prices/historical/{timestamp}/{coins}'
+        return self._handle_request(url)
+
+    def batch_historical_prices(self, coins):
+        url = f'{self.common_base_url}/batch/historical'
+        return self._handle_request(url)
+
+    def price_intervals(self, coins):
+        url = f'{self.common_base_url}/batch/intervals/{coins}'
+        return self._handle_request(url)
+
+    def price_percentage_change(self, coins):
+        url = f'{self.common_base_url}/percentage/{coins}'
+        return self._handle_request(url)
+
+    def price_first_record(self, coins):
+        url = f'{self.common_base_url}/prices/first/{coins}'
+        return self._handle_request(url)
+
+    def closest_block_to_timestamp(self, chain, timestamp):
+        url = f'{self.common_base_url}/block/{chain}/{timestamp}'
+        return self._handle_request(url)
+
+    # stablecoins section
+    def list_stablecoins(self):
+        url = f'{self.common_base_url}/stablecoins'
+        return self._handle_request(url)
+
+    def stablecoins_charts_all(self):
+        url = f'{self.common_base_url}/stablecoins/charts/all'
+        return self._handle_request(url)
+
+    def stablecoins_charts_by_chain(self, chain):
+        url = f'{self.common_base_url}/stablecoins/charts/{chain}'
+        return self._handle_request(url)
+
+    def historical_stablecoin(self, asset):
+        url = f'{self.common_base_url}/stablecoin/{asset}'
+        return self._handle_request(url)
+
+    def current_stablecoin_chains(self):
+        url = f'{self.common_base_url}/stablecoin/chains'
+        return self._handle_request(url)
+
+    def historical_stablecoin_prices(self):
+        url = f'{self.common_base_url}/stablecoin/prices'
+        return self._handle_request(url)
+
+    # yields section
+    def latest_pool_data(self, pools):
+        url = f'{self.common_base_url}/pools'
+        return self._handle_request(url)
+
+    def historical_pool_apys_tvls(self, pool):
+        url = f'{self.common_base_url}/chart/{pool}'
+        return self._handle_request(url)
+
+    # abi-decoder section
+    def fetch_signature_function(self, signature):
+        url = f'{self.common_base_url}/fetch/signature/{signature}'
+        return self._handle_request(url)
+
+    def fetch_contract_signature(self, contract, address):
+        url = f'{self.common_base_url}/fetch/contract/{contract}/{address}'
+        return self._handle_request(url)
+
+    # bridges section
+    def list_all_bridges(self):
+        url = f'{self.common_base_url}/bridges'
+        return self._handle_request(url)
+
+    def bridge_summary(self, id):
+        url = f'{self.common_base_url}/bridge/{id}'
+        return self._handle_request(url)
+
+    def bridge_volume_by_chain(self, chain):
+        url = f'{self.common_base_url}/bridge/volume/{chain}'
+        return self._handle_request(url)
+
+    def bridge_days_stats(self, timestamp, chain):
+        url = f'{self.common_base_url}/bridge/days/stats/{timestamp}/{chain}'
+        return self._handle_request(url)
+
+    def bridge_transactions(self, id):
+        url = f'{self.common_base_url}/transactions/{id}'
+        return self._handle_request(url)
 
 
 if __name__ == '__main__':
