437dab55c3b56dcf926e44b7a0d28837ed964737
Weston McDonald <weston.mcdonald@blockfills.com>
2024-02-09 16:20:51 -0600

    adding endpoints

--- defillama.py
+++ defillama.py
@@ -24,12 +24,12 @@ class DefiLlama():
         self.bridges_base_url = 'https://bridges.llama.fi'
 
 
-    def _handle_request(self, url, dataframe=True, **kwargs):
+    def _handle_request(self, url, return_dataframe=False, **kwargs):
 
         try:
             response = requests.get(url, **kwargs)
             response.raise_for_status()
-            if dataframe:
+            if return_dataframe:
                 return pd.DataFrame(response.json())
             else:
                 return response.json()
@@ -44,17 +44,36 @@ class DefiLlama():
             logging.error("Error during request: ", e)
         return None
                 
-            
 
     def protocols(self):
-        
-        url = f'{self.tvl_base_url}/protocols'
-        response 
-        response = pd.DataFrame(requests.get(url).json())
-        print(response)
+        url = f'{self.common_base_url}/protocols'
+        return self._handle_request(url)
+
+    def historical_tvl_full(self, protocol):
+        url = f'{self.common_base_url}/protocol/{protocol}'
+        return self._handle_request(url)
+
+    def historical_tvls(self):
+        url = f'{self.common_base_url}/historicalChainTvl'
+        return self._handle_request(url)
+
+    def historical_tvl(self, chain):
+        url = f'{self.common_base_url}/historicalChainTvl/{chain}'
+        return self._handle_request(url)
+
+    def current_tvl(self, protocol):
+        url = f'{self.common_base_url}/tvl/{protocol}'
+        return self._handle_request(url)
+
+    def current_tvls(self):
+        url = f'{self.common_base_url}/tvl/{protocol}'
+        return self._handle_request(url)
+
+
 
 
 if __name__ == '__main__':
 
     dl = DefiLlama()
-    dl.protocols()
+    tvl_protocols = dl.tvl_protocols()
+
